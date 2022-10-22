d645571394a82c73bbdb7d98af0341e9a1bb7217
akmalluthfi <akmalluthfi19@gmail.com>
2022-10-22 21:03:17 +0700

    showing chirps

--- blade-chirper/app/Http/Controllers/ChirpController.php
+++ blade-chirper/app/Http/Controllers/ChirpController.php
@@ -14,7 +14,9 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        return view('chirps.index');
+        return view('chirps.index', [
+            'chirps' => Chirp::with('user')->latest()->get(),
+        ]);
     }
 
     /**
--- blade-chirper/app/Models/Chirp.php
+++ blade-chirper/app/Models/Chirp.php
@@ -12,4 +12,9 @@ class Chirp extends Model
     protected $fillable = [
         'message'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
