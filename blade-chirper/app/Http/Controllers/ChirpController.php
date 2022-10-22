f68dc95986518835519aa33988573ef7447d111e
akmalluthfi <akmalluthfi19@gmail.com>
2022-10-22 21:43:43 +0700

    deleting chirps

--- blade-chirper/app/Http/Controllers/ChirpController.php
+++ blade-chirper/app/Http/Controllers/ChirpController.php
@@ -100,6 +100,10 @@ class ChirpController extends Controller
      */
     public function destroy(Chirp $chirp)
     {
-        //
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
     }
 }
