2c02cbd588b21420287576d2c6b30d60ef81580a
akmalluthfi <akmalluthfi19@gmail.com>
2022-10-22 21:24:58 +0700

    editing chirp

--- blade-chirper/app/Http/Controllers/ChirpController.php
+++ blade-chirper/app/Http/Controllers/ChirpController.php
@@ -65,7 +65,11 @@ class ChirpController extends Controller
      */
     public function edit(Chirp $chirp)
     {
-        //
+        $this->authorize('update', $chirp);
+
+        return view('chirps.edit', [
+            'chirp' => $chirp
+        ]);
     }
 
     /**
@@ -77,7 +81,15 @@ class ChirpController extends Controller
      */
     public function update(Request $request, Chirp $chirp)
     {
-        //
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255'
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
